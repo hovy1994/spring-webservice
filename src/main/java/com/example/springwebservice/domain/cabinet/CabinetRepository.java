d731377cd213f1e093fd9c00f858695fa34890c0
Jimin Sohn <sjm7396@naver.com>
2020-05-24 19:56:39 +0900

    Create CabinetRepository.java

---
+++ src/main/java/com/example/springwebservice/domain/cabinet/CabinetRepository.java
@@ -0,0 +1,6 @@
+package com.example.springwebservice.domain.cabinet;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CabinetRepository  extends JpaRepository<Cabinet, Long> {
+}
