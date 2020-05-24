a1d0897c0e6c1cf082a77a1d05d99f73aa7cb4b6
Jimin Sohn <sjm7396@naver.com>
2020-05-24 20:23:37 +0900

    Update RentRepository.java

--- src/main/java/com/example/springwebservice/domain/rent/RentRepository.java
+++ src/main/java/com/example/springwebservice/domain/rent/RentRepository.java
@@ -1,6 +1,6 @@
 package com.example.springwebservice.domain.rent;
 
-import com.example.springwebservice.domain.posts.Posts;
+import com.example.springwebservice.domain.rent.Rent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RentRepository  extends JpaRepository<Rent, Long> {
