c335c6824dcaf88bcb5c976e82b659e09695e973
Yu <hovy1994@naver.com>
2020-05-24 20:11:48 +0900

    수정

--- src/main/java/com/example/springwebservice/domain/rent/RentRepository.java
+++ src/main/java/com/example/springwebservice/domain/rent/RentRepository.java
@@ -1,13 +1,7 @@
 package com.example.springwebservice.domain.rent;
 
-<<<<<<< HEAD
 import com.example.springwebservice.domain.posts.Posts;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RentRepository  extends JpaRepository<Rent, Long> {
-=======
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RentRepository extends JpaRepository<Rent, Long> {
->>>>>>> 3f2ba606c46c3f5d53cf84ec2dd2c5ed2c913952
 }
