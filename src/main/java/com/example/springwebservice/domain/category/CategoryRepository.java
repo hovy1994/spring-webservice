1782fa7e3f07449c6967660b1cab1c2d92c4d36c
Jimin Sohn <sjm7396@naver.com>
2020-05-24 19:57:37 +0900

    Update CategoryRepository.java

--- src/main/java/com/example/springwebservice/domain/category/CategoryRepository.java
+++ src/main/java/com/example/springwebservice/domain/category/CategoryRepository.java
@@ -3,5 +3,5 @@ package com.example.springwebservice.domain.category;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository  extends JpaRepository<Category, Long> {
+public interface CategoryRepository  extends JpaRepository<Category, Long> {
 }
