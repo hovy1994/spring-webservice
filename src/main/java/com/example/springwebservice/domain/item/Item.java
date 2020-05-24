11303b81535462c894a969bf61fb2291f4b0ba1d
Yu <hovy1994@naver.com>
2020-05-24 20:06:39 +0900

    item

--- src/main/java/com/example/springwebservice/domain/item/Item.java
+++ src/main/java/com/example/springwebservice/domain/item/Item.java
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
 package com.example.springwebservice.domain.item;
 
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-=======
-package com.example.springwebservice.domain.Item;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-
-import lombok.*;
-
->>>>>>> 3f2ba606c46c3f5d53cf84ec2dd2c5ed2c913952
 
 import javax.persistence.*;
 
@@ -20,15 +11,10 @@ import javax.persistence.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
-<<<<<<< HEAD
 public class Item {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-=======
-public class Item{
-    @Id
->>>>>>> 3f2ba606c46c3f5d53cf84ec2dd2c5ed2c913952
     private Integer ITEM_IDX;
 
     private Integer CATEGORY_IDX;
@@ -41,7 +27,6 @@ public class Item{
     private String ITEM_NAME;
 
     @Builder
-<<<<<<< HEAD
     public Item(Integer ITEM_IDX, Integer CATEGORY_IDX,Integer ITEM_NUM,Integer RENT_PRICE,String ITEM_NAME) {
         this.ITEM_IDX=ITEM_IDX;
         this.CATEGORY_IDX = CATEGORY_IDX;
@@ -49,14 +34,4 @@ public class Item{
         this.RENT_PRICE=RENT_PRICE;
         this.ITEM_NAME=ITEM_NAME;
     }
-=======
-    public Item(Integer ITEM_IDX, Integer CATEGORY_IDX,Integer ITEM_NUM, Integer RENT_PRICE, String ITEM_NAME) {
-        this.ITEM_IDX=ITEM_IDX;
-        this.CATEGORY_IDX = CATEGORY_IDX;
-        this.ITEM_NUM=ITEM_NUM;
-        this.RENT_PRICE = RENT_PRICE;
-        this.ITEM_NAME=ITEM_NAME;
-    }
-
->>>>>>> 3f2ba606c46c3f5d53cf84ec2dd2c5ed2c913952
 }
