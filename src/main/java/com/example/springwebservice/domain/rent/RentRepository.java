3f2ba606c46c3f5d53cf84ec2dd2c5ed2c913952
Jimin Sohn <sjm7396@naver.com>
2020-05-24 19:58:51 +0900

    Create RentRepository.java

---
+++ src/main/java/com/example/springwebservice/domain/rent/RentRepository.java
@@ -0,0 +1,6 @@
+package com.example.springwebservice.domain.rent;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RentRepository extends JpaRepository<Rent, Long> {
+}
