b8a7293d4d987915b5ea256e06bbfcc75b97b0d4
Jimin Sohn <sjm7396@naver.com>
2020-05-26 16:12:46 +0900

    Update SpringWebserviceApplication.java

--- src/main/java/com/example/springwebservice/SpringWebserviceApplication.java
+++ src/main/java/com/example/springwebservice/SpringWebserviceApplication.java
@@ -3,7 +3,9 @@ package com.example.springwebservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @EnableJpaAuditing
 @SpringBootApplication
 public class SpringWebserviceApplication {
