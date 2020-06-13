9f364d495836db0e62df4d4a253e37f217c8f5a9
Gabriel <sjm7396@naver.com>
2020-06-13 13:30:30 +0900

    To build kakaoPay

---
+++ src/main/java/com/example/htmltest/KakaoPayReadyVO.java
@@ -0,0 +1,11 @@
+package com.example.htmltest;
+
+import java.util.Date;
+import lombok.Data;
+
+@Data
+public class KakaoPayReadyVO {
+    //response
+    private String tid, next_redirect_pc_url;
+    private Date created_at;
+}
