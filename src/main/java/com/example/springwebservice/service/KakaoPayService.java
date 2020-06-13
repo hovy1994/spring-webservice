1cfa10054be1647e2afac28bc97c3bb3f4ecc48b
Yu <hovy1994@naver.com>
2020-06-13 20:37:58 +0900

    수정

--- src/main/java/com/example/springwebservice/service/KakaoPayService.java
+++ src/main/java/com/example/springwebservice/service/KakaoPayService.java
@@ -38,35 +38,48 @@ public class KakaoPayService {
     private PaymentRepository paymentRepository;
 
     public String kakaoPayReady(RentalRequestInfo info) {
+        System.out.println("여기10");
         RestTemplate restTemplate = new RestTemplate();
-
+        System.out.println("여기11");
         // 서버로 요청할 Heaader
         HttpHeaders headers = new HttpHeaders();
         headers.add("Authorization", "KakaoAK " + "16dbafbd576dc51ec0cb7a3ade20a721");
         headers.add("Accept", MediaType.APPLICATION_JSON_UTF8_VALUE);
         headers.add("Content-Type", MediaType.APPLICATION_FORM_URLENCODED_VALUE + ";charset=UTF-8");
 
+        System.out.println("여기15");
         List<Payment> paymentList = paymentRepository.findAll();
 
+        System.out.println("여기19");
         // 서버로 요청할 Body
         MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
-        params.add("cid", "123");
-        System.out.println("여기1");
-        params.add("tid",String.valueOf(paymentList.size()+1));
+//        params.add("cid", "123");
+//        System.out.println("여기1");
+//        params.add("tid",String.valueOf(paymentList.size()+1));
+//        params.add("partner_order_id", "1004");
+//        System.out.println("여기2");
+//        params.add("partner_user_id", info.getUser_id());
+//        System.out.println("여기3");
+//        params.add("item_name", String.valueOf(info.getItem_idx()));
+//        params.add("quantity","1");
+//        System.out.println("여기4");
+//        params.add("total_amount", String.valueOf(info.getTotal_amount()));
+//        System.out.println("여기5");
+//        params.add("tax_free_amount", "0");
+
+        params.add("cid", "TC0ONETIME");
         params.add("partner_order_id", "1004");
-        System.out.println("여기2");
-        params.add("partner_user_id", info.getUser_id());
-        System.out.println("여기3");
-        params.add("item_name", String.valueOf(info.getItem_idx()));
-        params.add("quantity","1");
-        System.out.println("여기4");
-        params.add("total_amount", String.valueOf(info.getTotal_amount()));
-        System.out.println("여기5");
+        params.add("partner_user_id", "BEBLET_USER1");
+        params.add("item_name", "Tablet1");
+        params.add("quantity", "1");
+        params.add("total_amount", "15000");
         params.add("tax_free_amount", "0");
+
         params.add("approval_url", "http://13.125.236.67:8080/kakaoPaySuccess");
         params.add("cancel_url", "http://13.125.236.67:8080/kakaoCancel");
         params.add("fail_url", "http://13.125.236.67:8080/kakaoFail");
 
+        System.out.println("여기20");
         HttpEntity<MultiValueMap<String, String>> body = new HttpEntity<MultiValueMap<String, String>>(params, headers);
 
         try {
