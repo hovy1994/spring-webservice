429198b152cf061b69d34a9e8047d57afbc241fc
Yu <hovy1994@naver.com>
2020-06-23 18:47:50 +0900

    취소 구현

--- src/main/java/com/example/springwebservice/domain/controller/RentalController.java
+++ src/main/java/com/example/springwebservice/domain/controller/RentalController.java
@@ -93,12 +93,12 @@ public class RentalController {
     }
 
     // 사용자의 결제 내역 리스트 리턴
-    @RequestMapping(value="/returnPaymentList",method={ RequestMethod.GET, RequestMethod.POST })
-    public List<Payment> returnPayment(@RequestBody RentalRequestInfo info){  // null 리턴되면 결제 제대로 안된 것
-        List<Payment> payment=kakaoPayService.returnPaymentList(info.getUser_id());
-
-        return payment;
-    }
+//    @RequestMapping(value="/returnPaymentList",method={ RequestMethod.GET, RequestMethod.POST })
+//    public List<Payment> returnPayment(@RequestBody RentalRequestInfo info){  // null 리턴되면 결제 제대로 안된 것
+//        List<Payment> payment=kakaoPayService.returnPaymentList(info.getUser_id());
+//
+//        return payment;
+//    }
 
     //
     @RequestMapping(value="/CancleOrReturn",method={ RequestMethod.GET, RequestMethod.POST })
