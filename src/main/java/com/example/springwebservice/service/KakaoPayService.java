f4c5fd393a1ee49ac70dd969c5dd68628cf3f688
Yu <hovy1994@naver.com>
2020-06-25 16:29:19 +0900

    카카오페이 수정

--- src/main/java/com/example/springwebservice/service/KakaoPayService.java
+++ src/main/java/com/example/springwebservice/service/KakaoPayService.java
@@ -87,7 +87,7 @@ public class KakaoPayService {
         try {
             kakaoPayReadyVO = restTemplate.postForObject(new URI(HOST + "/v1/payment/ready"), body, KakaoPayReadyVO.class);
             log.info("" + kakaoPayReadyVO);
-            return kakaoPayReadyVO.getNext_redirect_pc_url();
+            return kakaoPayReadyVO.getNext_redirect_mobile_url();
         } catch (RestClientException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
